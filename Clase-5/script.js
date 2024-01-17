667f48d5216fc534c4a10b9586af5e635fb7d2ce
Victoria-Lopez-dev <72264965+Victoria-Lopez-dev@users.noreply.github.com>
2024-01-17 16:34:41 -0300

    clase 5

--- Clase-5/script.js
+++ Clase-5/script.js
@@ -4,6 +4,10 @@ const elipse=document.querySelector("ellipse");
 const mover=()=>{
     poligono.classList.toggle("moverLugar")
 }
-elipse.addEventListener("click",()=>{
+elipse.addEventListener("mouseover",()=>{
     elipse.style.animation="color 3s 2"
+})
+
+elipse.addEventListener("click",()=>{
+   console.log("HOLLA")
 })
